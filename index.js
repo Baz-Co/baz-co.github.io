3ce7161cd38ca06091e88fbdbeef386e6350e22f
Baz-Co <shehbaz.sherwani@gmail.com>
2018-03-02 19:16:02 -0600

    Add External Link Scroll Animation

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const logo = document.querySelector('.logo')
 const navigationDiv = document.querySelector('.navigation')
 const topOfNavigation = navigationDiv.offsetTop
 const siteNav = document.querySelector('#site-nav')
 const externalLinks = document.querySelector('#external-links')
 const main = document.querySelector('main')
+const scrollDown = document.querySelector('#scroll-down')
 
 function handleScroll(e) {
   const scrollY = window.scrollY
-  // console.log({scrollY, topOfNavigation})
+
+  if (scrollY === 0) {
+    document.body.appendChild(scrollDown)
+  } else {
+    if (document.querySelector('#scroll-down')) {
+      document.body.removeChild(scrollDown)
+    }
+  }
+
   if (scrollY >= topOfNavigation){
-    // main.style.paddingTop = siteNav.offsetHeight + 'px'
+    main.style.paddingTop = siteNav.offsetHeight + 'px'
     document.body.classList.add('fixed-nav')
   } else {
     main.style.paddingTop = 0
@@ -17,4 +25,10 @@ function handleScroll(e) {
   }
 }
 
+function forceNextPage(e) {
+  // TODO: don't hard code 
+  window.scrollBy(0, 450);
+}
+
 window.addEventListener('scroll', handleScroll)
+scrollDown.addEventListener('click', forceNextPage)
